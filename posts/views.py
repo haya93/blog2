aaa88e6989ac67c904edcb0c9afc1ecc912f7adc
haya93 <rosybutter@hotmail.com>
2017-07-23 20:41:14 +0300

    added links

--- posts/views.py
+++ posts/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from.models import Post
-from django.shortcuts import get_object_404 
+from django.shortcuts import get_object_or_404 
 def home(request):
 	return HttpResponse("<h1> Hello world </h1>")
 def post_create(request):
-	obj = get_object_or_or_404(post, id=100000)
+	
 	context = {
 	"title"
 	}
 	return render(request, 'create.html', context)
 
-def post_detail(request): 
-	objects_list=Post.objects.all()
-	objects_get=Post.objects.get(id=1)
+def post_detail(request, post_id): 
+	obj = get_object_or_404(Post, id=post_id)
+	
 	context = {
-		"objects_list": objects_list,
-		"title": "List",
-		"user": request.user,
-		"objects_get": objects_get,
+		"instance": obj,
 	}
-	return render(request, 'create2.html', context)
+	return render(request, 'post_detail.html', context)
 
 def post_list(request):
-	return render(request, 'create3.html', {})
+	
+	objects_list=Post.objects.all()
+	context = {
+	"post_list": objects_list,
+	}
+	return render(request, 'post_list.html', context)
 
 def post_update(request):
 	return render(request, 'create4.html', {})
