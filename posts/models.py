e40768a8bb2b79474170f637ffa5c5bf89913f13
haya93 <rosybutter@hotmail.com>
2017-07-24 19:49:54 +0300

    I saved new url

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
@@ -8,3 +8,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+def get_absolute_url(self):
+	return reverse("posts:detail", kwargs={"post_id": self.id})
