9aa90db1bf2f847d4b8c69d1c0300566c96c0e44
haya93 <rosybutter@hotmail.com>
2017-07-26 18:57:10 +0300

    used bootstrap for styling

--- posts/models.py
+++ posts/models.py
@@ -8,5 +8,9 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+	
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"post_id": self.id})
+
+	class Meta:
+		ordering = ['-timestamp', '-updated']
